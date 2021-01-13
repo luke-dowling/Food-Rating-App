996172e44996cdeaef598990fbcba4ae420a8c7b
AdrianValdes <adrianvaldeshdz@gmail.com>
2021-01-13 15:47:34 +0100

    added icon

--- food-rating-app/src/components/Nav.jsx
+++ food-rating-app/src/components/Nav.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { Flex, Box, Heading, Spacer, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import favicon from "../assets/favicon.ico";
 
 const Nav = () => {
   return (
     <Flex p="3">
       <Box p="2">
         <Heading size="md" color="red.600">
+          <img
+            alt="icon"
+            src={favicon}
+            height={36}
+            width={36}
+            style={{ display: "inline", paddingRight: "10px" }}
+          />
           @CommunityChef
         </Heading>
       </Box>
       <Spacer />
-      <Box>
+      <Box p="2">
         <Link to="signup">
           <Button colorScheme="teal" mr="5">
             Sign Up
